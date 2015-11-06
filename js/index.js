38b0f4c6b9b62b2e31fc22992a188d91928572a7
Franklyn Zhou <zhoufranklyn@gmail.com>
2015-11-06 16:50:37 -0500

    update index.html

--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
+width = 0;
+length = 0;
 $(document).ready(function() {
-	var width = prompt("Enter grid width");
-	var length = prompt("Enter grid length");
+	width = prompt("Enter grid width");
+	length = prompt("Enter grid length");
 	for (var i=1; i<=width; i++) {
 		for (var j=1; j<=length; j++) {
-			var $box = $("<div />", {class: "gridBox"});
+			$box = $("<div />", {class: "gridBox"});
 			$(".wrapper").append($box);
 		}
+		$(".wrapper").append("<br />");
 	}
+	$(".gridBox").mouseenter(function() {
+		$(this).css("background-color", "#3366FF");
+	});
+	$(".gridBox").click(function() {
+		$(this).css("background-color", "#E4E4E4");
+	});
 });
 
-function makeGrid(columns, rows) {
-	for (var i=1; i<=columns; i++) {
-		for (var j=1; j<=rows; j++) {
-			var $box = $("<div />", {class: "gridBox"});
-			$("").append($box);
-			if (j === rows) {
-				document.write("<br>");
-			}
+function newGrid() {
+	width = prompt("Enter grid width");
+	length = prompt("Enter grid length");
+	$(".wrapper").empty();
+	for (var i=1; i<=width; i++) {
+		for (var j=1; j<=length; j++) {
+			$box = $("<div />", {class: "gridBox"});
+			$(".wrapper").append($box);
 		}
+		$(".wrapper").append("<br />");
 	}
-};
+	$(".gridBox").mouseenter(function() {
+		$(this).css("background-color", "#3366FF");
+	});
+	$(".gridBox").click(function() {
+		$(this).css("background-color", "#E4E4E4");
+	});
+}
